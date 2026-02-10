62b13c3695f36d3924e54d6316f316dd126af3bf
NabilMShandy <nmuhammadshandy@gmail.com>
2026-02-10 07:53:59 +0700

    delete

--- Codewars (Python) Solution/kyu 7 GrowthPopulation.py
+++ Codewars (Python) Solution/kyu 7 GrowthPopulation.py
@@ -14,17 +14,6 @@ def nb_year(p0, percent, aug, p):
 print(nb_year(1500, 5, 100, 5000))
 
 
-
-
-
-
-
-
-
-
-
-
-
 # In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater than or equal to p = 1200 inhabitants?
 
 # At the end of the first year there will be: 
